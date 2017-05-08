0cc3c1dd7718b4d3b84852ab09409ec6e685f126
lama tak jumpa <maxndra@gmail.com>
2017-05-08 21:02:47 +0800

    input dengan intreger

---
+++ coba.rb
@@ -0,0 +1,12 @@
+puts "Tulis Nama"
+
+nama = gets.chomp
+
+puts "Sandinya apa?"
+
+password = gets.chomp
+if (password == "dosaya")
+    puts "saya " + nama
+else
+    puts "jangan mengaku " + nama
+end
---
+++ input2.rb
@@ -0,0 +1,7 @@
+puts "mau pesan apa?"
+menu = gets.chomp
+
+puts "pesan berapa banyak?"
+jumlah = gets.chomp.to_i
+
+p "jadi anda pesan #{menu} sebanyak #{jumlah} dengan biaya total #{jumlah *921} dolar"
