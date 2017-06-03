d7d63e3027aba05535326882ac966edbd5767e00
maxndra <maxndra@gmail.com>
2017-06-03 14:26:36 +0800

    Add files via upload

---
+++ Buzzwire.ino.ino
@@ -0,0 +1,78 @@
+const int hit = 6;
+const int button = 7;
+const int led = 2;
+const int led1 = 3;
+const int led2 = 4;
+const int led3 = 5;
+int lives = 3;
+int state = 0;
+int gameRunning = false;
+
+void setup()
+{
+  pinMode(hit, INPUT);
+  pinMode(button, INPUT);
+  pinMode(led, OUTPUT);
+  pinMode(lives, INPUT);
+  pinMode(led1, OUTPUT);
+  pinMode(led2, OUTPUT);
+  pinMode(led3, OUTPUT);
+  Serial.begin(9600);
+}
+
+void loop()
+{
+  state = digitalRead(button);
+  if (state == HIGH)
+  {
+    gameRunning = true;
+  }
+  else if (lives = 0)
+  {
+    gameRunning = false;
+  }
+
+  while(gameRunning)
+  {
+    state = digitalRead(hit);
+    if (state == HIGH)
+    {
+      digitalWrite(led, HIGH);
+      delay(1500);
+    }
+    else
+    {
+      digitalWrite(led, LOW);
+    }
+  
+    state = digitalRead(hit);
+    if (state == HIGH)
+    {
+      lives -= 1;
+    }
+    if (lives = 3)
+    {
+      digitalWrite(led1, HIGH);
+      digitalWrite(led2, HIGH);
+      digitalWrite(led3, HIGH);
+    }
+    else if (lives = 2)
+    {
+      digitalWrite(led1, LOW);
+      digitalWrite(led2, HIGH);
+      digitalWrite(led3, HIGH);
+    }
+    else if (lives = 1)
+    {
+      digitalWrite(led1, LOW);
+      digitalWrite(led2, LOW);
+      digitalWrite(led3, HIGH);
+    }
+    else if (lives = 0)
+    {
+      digitalWrite(led1, LOW);
+      digitalWrite(led2, LOW);
+      digitalWrite(led3, LOW);
+    }
+  }
+}
